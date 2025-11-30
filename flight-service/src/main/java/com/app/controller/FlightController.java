b77c9938a22868b57ddac2ec0b021ef85d6eef37
ashutosh <ashutoshashu2001@gmail.com>
2025-12-01 00:38:57 +0530

    Feature: added flightservice ,controller

---
+++ flight-service/src/main/java/com/app/controller/FlightController.java
@@ -0,0 +1,60 @@
+package com.app.controller;
+
+
+import com.app.dto.FlightRequestDTO;
+import com.app.dto.FlightResponseDTO;
+import com.app.dto.FlightSearchDTO;
+import com.app.model.Flight;
+import com.app.service.FlightService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/flight")
+public class FlightController {
+
+
+    @Autowired
+    private  FlightService flightService;
+
+    @PostMapping
+    public ResponseEntity<?> addFlight( @Valid @RequestBody FlightRequestDTO request) {
+        FlightResponseDTO response= flightService.addFlight(request);
+      return  new ResponseEntity<>(response, HttpStatus.CREATED); //note for future , responseEntity has a factory method that returns an object
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FlightResponseDTO>> getFlights(@Valid @RequestBody FlightRequestDTO request) {
+        return ResponseEntity.ok(flightService.getAllFlights());
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<FlightResponseDTO>> searchFlights(
+            @RequestParam String source,
+            @RequestParam String destination,
+            @RequestParam(required = false) String date) {
+
+        FlightSearchDTO searchDTO = new FlightSearchDTO(source, destination, date);
+        List<FlightResponseDTO> flights = flightService.searchFlights(searchDTO);
+
+        return ResponseEntity.ok(flights);
+    }
+
+    @GetMapping("/{flightNumber}")
+    public ResponseEntity<FlightResponseDTO> getFlightByNumber(
+            @PathVariable String flightNumber) {
+
+        FlightResponseDTO flight = flightService.getFlightByNumber(flightNumber);
+        return ResponseEntity.ok(flight);
+    }
+
+
+
+
+}
+
--- flight-service/src/main/java/com/app/repository/FlightRepository.java
+++ flight-service/src/main/java/com/app/repository/FlightRepository.java
@@ -2,15 +2,18 @@ package com.app.repository;
 
 import com.app.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface FlightRepository extends JpaRepository<Flight, Integer> {
 
     Optional<Flight> findByFlightNumber(String flightNumber);
 
     List<Flight> findBySourceAndDestinationAndDepartureTimeBetween(String source, String destination, LocalDateTime start, LocalDateTime end);
 
+    List<Flight> findBySourceAndDestination(String source, String destination);
 }
